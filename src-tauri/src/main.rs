a9d87547473899ace229d919035b75106af0210c
Sam McLeod <sammcj@users.noreply.github.com>
2023-11-25 17:19:50 +1100

    fix: fix clippy linting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use reqwest;
 use reqwest::Client;
 use serde_json::{json, Value};
 use tauri::command;
